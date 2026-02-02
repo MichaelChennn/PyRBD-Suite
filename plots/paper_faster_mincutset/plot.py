9df7355e8a47bdba51999b70bfc6c726a66fdf56
XuanBook14 <chenyaxuan123@gmail.com>
2026-02-02 11:16:34 +0100

    update plots for faster mcs

--- plots/paper_faster_mincutset/plot.py
+++ plots/paper_faster_mincutset/plot.py
@@ -20,8 +20,12 @@ def plot_1(plot_name, topologies=None):
     for topo in topologies:
         # Read the CSV file
         cnf = pd.read_csv(f"benchmark/mincutsets/results/cnf/{topo}_MCS_cnf.csv")
-        combination = pd.read_csv(f"benchmark/mincutsets/results/combination_matrix/{topo}_MCS_combination_matrix.csv")
-        minimalpaths = pd.read_csv(f"benchmark/pathsets/results/minimalpaths/{topo}_Pathsets_minimalpaths.csv")
+        combination = pd.read_csv(
+            f"benchmark/mincutsets/results/combination_matrix/{topo}_MCS_combination_matrix.csv"
+        )
+        minimalpaths = pd.read_csv(
+            f"benchmark/pathsets/results/minimalpaths/{topo}_Pathsets_minimalpaths.csv"
+        )
         # Sum the time columns
         cnf_time = cnf["cnf_time (second)"].sum()
         combination_time = combination["combination_matrix_time (second)"].sum()
@@ -42,25 +46,25 @@ def plot_1(plot_name, topologies=None):
     # x positions for grouped bars
     x = np.arange(len(topologies))
     ax1.bar(
-        x - width/2,
+        x - width / 2,
         minimalpaths_times,
         width,
         label="Finding MPSs",
         color="#08519c",
     )
     ax1.bar(
-        x - width/2,
+        x - width / 2,
         cnf_times,
         width,
-        label="Finding MCSs from MPSs (Improved)",
+        label="Finding MCSs from MPSs",
         color="#B2182B",
         bottom=minimalpaths_times,
     )
     ax1.bar(
-        x + width/2,
+        x + width / 2,
         combination_times,
         width,
-        label="Finding MCSs (Old)",
+        label="Finding MCSs (combinatorial)",
         color="#D57E26",
     )
 
@@ -85,7 +89,9 @@ def plot_1(plot_name, topologies=None):
 
     # Save the plot
     plt.tight_layout()
-    plt.savefig(f"plots/paper_faster_mincutset/{plot_name}.png", dpi=300, bbox_inches="tight")
+    plt.savefig(
+        f"plots/paper_faster_mincutset/{plot_name}.png", dpi=300, bbox_inches="tight"
+    )
     plt.close()
 
 
@@ -95,11 +101,13 @@ def plot_2(plot_name, topologies=None):
     for topo in topologies:
         # Read the CSV file
         cnf = pd.read_csv(f"benchmark/mincutsets/results/cnf/{topo}_MCS_cnf.csv")
-        shannon = pd.read_csv(f"benchmark/mincutsets/results/shannon/{topo}_MCS_shannon.csv")
+        shannon = pd.read_csv(
+            f"benchmark/mincutsets/results/shannon/{topo}_MCS_shannon.csv"
+        )
         # Sum the time columns
         cnf_time.append(cnf["cnf_time (second)"].sum())
         shannon_time.append(shannon["shannon_time (second)"].sum())
-    
+
     # Draw the plot
     fig, ax1 = plt.subplots(figsize=FIG_SIZE)
     width = 0.4  # Bar width for each series
@@ -110,14 +118,14 @@ def plot_2(plot_name, topologies=None):
         x - width / 2,
         cnf_time,
         width,
-        label="Finding MCSs",
+        label=r"$\it{Fast}$-${MCS}$",
         color="#4E79A7",
     )
     ax1.bar(
         x + width / 2,
         shannon_time,
         width,
-        label="Finding MCSs (Shannon Expansion)",
+        label="Shannon expansion",
         color="#F28E2B",
     )
 
@@ -142,8 +150,11 @@ def plot_2(plot_name, topologies=None):
 
     # Save the plot
     plt.tight_layout()
-    plt.savefig(f"plots/paper_faster_mincutset/{plot_name}.png", dpi=300, bbox_inches="tight")
+    plt.savefig(
+        f"plots/paper_faster_mincutset/{plot_name}.png", dpi=300, bbox_inches="tight"
+    )
     plt.close()
+
 
 # plot only mps time
 def plot_3(plot_name, topologies=None):
@@ -151,7 +162,9 @@ def plot_3(plot_name, topologies=None):
     for topo in topologies:
         # G, _, _ = read_graph(f"topologies/{topo}", topo)
         # print(f"Processing topology: {topo} with {len(G.nodes())} nodes and {len(G.edges())} edges")
-        df = pd.read_csv(f"benchmark/pathsets/results/minimalpaths/{topo}_Pathsets_minimalpaths.csv")
+        df = pd.read_csv(
+            f"benchmark/pathsets/results/minimalpaths/{topo}_Pathsets_minimalpaths.csv"
+        )
         # add the sum of "minimalpaths_time (second)" to mps_time
         total_time = df["minimalpaths_time (second)"].sum()
         mps_time.append(total_time)
@@ -159,10 +172,10 @@ def plot_3(plot_name, topologies=None):
     # Draw the plot
     fig, ax1 = plt.subplots(figsize=FIG_SIZE)
     width = 0.5  # Bar width for each series
-    
+
     # x positions for grouped bars
     x = np.arange(len(topologies))
-    
+
     ax1.bar(
         x,
         mps_time,
@@ -170,13 +183,11 @@ def plot_3(plot_name, topologies=None):
         label="Finding MPSs",
         color="#4E79A7",
     )
-    
+
     # Set the x-ticks and labels
     ax1.set_xticks(x)
     # ax1.set_xticklabels(topologies, fontsize=FONT_SIZE - 2, rotation=ROTATION)
-    labels = ax1.set_xticklabels(
-        topologies, fontsize=FONT_SIZE - 2, rotation=ROTATION
-    )
+    labels = ax1.set_xticklabels(topologies, fontsize=FONT_SIZE - 2, rotation=ROTATION)
 
     # Custom adjustment for Germany_17
     for label in labels:
@@ -193,15 +204,16 @@ def plot_3(plot_name, topologies=None):
         loc="upper left",
         fontsize=15,
     )
-    
+
     # Save the plot
     plt.tight_layout()
-    plt.savefig(f"plots/paper_faster_mincutset/{plot_name}.png", dpi=300, bbox_inches="tight")
+    plt.savefig(
+        f"plots/paper_faster_mincutset/{plot_name}.png", dpi=300, bbox_inches="tight"
+    )
     plt.close()
 
 
-
-topos_1= [
+topos_1 = [
     "Abilene",
     "polska",
     "HiberniaUk",
@@ -219,7 +231,7 @@ topos_1= [
     # "zib54",
 ]
 
-topos_2_1= [
+topos_2_1 = [
     "Abilene",
     "polska",
     "HiberniaUk",
@@ -237,13 +249,9 @@ topos_2_1= [
     # "zib54",
 ]
 
-topos_2_2= [
-    "india35",
-    "jonas-us-ca",
-    "pioro40"
-]
+topos_2_2 = ["india35", "jonas-us-ca", "pioro40"]
 
-topos_3= [
+topos_3 = [
     "Abilene",
     "polska",
     "HiberniaUk",
@@ -265,4 +273,3 @@ plot_1("plot_1", topologies=topos_1)
 plot_2("plot_2_1", topologies=topos_2_1)
 plot_2("plot_2_2", topologies=topos_2_2)
 plot_3("plot_3", topologies=topos_3)
-
